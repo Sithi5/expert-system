8d2f85565eb69d7d1675a44e6f0b6d80c17268d6
judumay <judumay@student.42.fr>
2021-01-07 16:14:57 +0100

    updated README

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
-## TO DO LIST
-| TO DO | STATUS|
-| ------------------ |:----:|
-| Parsing | &#9744;
-| Tree Creation | &#9744;
-| Backward chaining | &#9744;
-| battery of tests | &#9744;
-| Bonus | &#9744;
+# TODO LIST
+
+## Parsing
+- &#9744; Validation du fichier/ chaque lignes
+> 2-3 sections (queries, facts), checker parentheses
+- &#9745; Transformer les rules en **Notation Polonaise Inversé**
+- &#9745; Stocker les données du file
+
+## Nodes
+- &#9745; Créer la classe Tree avec les données
+- &#9745; Set les LetterNodes
+- &#9745; Set les states
+- &#9744; Set les connexions
+
+## Verification
+- &#9744; Checker loop
+- &#9744; Checker Duplicatas
+
+## Resolution
+- &#9744; Parcourir l'arbre et set les states
+- &#9744; Return / Print les resultats
+
+## Test
+- &#9744; Creer multiples de tests
+
+## Bonus
--- tests/test_expert-system.py
+++ tests/test_expert-system.py
@@ -3,30 +3,20 @@ from pathlib import Path
 
 
 def test_simple_1():
-    assert "B resolved as True" == main(
-        open(Path(__file__ + "/../" + "files/test_simple_1").absolute()), False
-    )
+	assert "B resolved as True" == main(open(Path(__file__ + "/../" + "files/test_simple_1").absolute()), False)
 
 
 def test_simple_2():
-    assert "B resolved as True" == main(
-        open(Path(__file__ + "/../" + "files/test_simple_2").absolute()), False
-    )
+	assert "B resolved as True" == main(open(Path(__file__ + "/../" + "files/test_simple_2").absolute()), False)
 
 
 def test_simple_3():
-    assert "B resolved as True" == main(
-        open(Path(__file__ + "/../" + "files/test_simple_3").absolute()), False
-    )
+	assert "B resolved as True" == main(open(Path(__file__ + "/../" + "files/test_simple_3").absolute()), False)
 
 
 def test_simple_4():
-    assert "B resolved as True" == main(
-        open(Path(__file__ + "/../" + "files/test_simple_4").absolute()), False
-    )
+	assert "B resolved as True" == main(open(Path(__file__ + "/../" + "files/test_simple_4").absolute()), False)
 
 
 def test_simple_5():
-    assert "B resolved as True" == main(
-        open(Path(__file__ + "/../" + "files/test_simple_5").absolute()), False
-    )
+	assert "B resolved as True" == main(open(Path(__file__ + "/../" + "files/test_simple_5").absolute()), False)
