6d7397b31a7702a6f425eef602006b2560799314
Sithi5 <ma.sithis@gmail.com>
2021-01-07 15:55:57 +0100

    push the fix for absolute path for tests

--- tests/test_expert-system.py
+++ tests/test_expert-system.py
@@ -1,16 +1,32 @@
 from expert_system import main
+from pathlib import Path
+
 
 def test_simple_1():
-	assert "B resolved as True" == main(open("files/test_simple_1"), False)
+    assert "B resolved as True" == main(
+        open(Path(__file__ + "/../" + "files/test_simple_1").absolute()), False
+    )
+
 
 def test_simple_2():
-	assert "B resolved as True" == main(open("files/test_simple_2"), False)
+    assert "B resolved as True" == main(
+        open(Path(__file__ + "/../" + "files/test_simple_2").absolute()), False
+    )
+
 
 def test_simple_3():
-	assert "B resolved as True" == main(open("files/test_simple_3"), False)
+    assert "B resolved as True" == main(
+        open(Path(__file__ + "/../" + "files/test_simple_3").absolute()), False
+    )
+
 
 def test_simple_4():
-	assert "B resolved as True" == main(open("files/test_simple_4"), False)
+    assert "B resolved as True" == main(
+        open(Path(__file__ + "/../" + "files/test_simple_4").absolute()), False
+    )
+
 
 def test_simple_5():
-	assert "B resolved as True" == main(open("files/test_simple_5"), False)
+    assert "B resolved as True" == main(
+        open(Path(__file__ + "/../" + "files/test_simple_5").absolute()), False
+    )
