9f84fd0435e364a81724fa82142c1ec1c252a9d6
Sithi5 <ma.sithis@gmail.com>
2021-01-08 13:06:45 +0100

    pushing a bonus test

--- tests/test_expert-system.py
+++ tests/test_expert-system.py
@@ -2,21 +2,43 @@ from expert_system import main
 from pathlib import Path
 
 
+def test_subject():
+    assert "B resolved as True" == main(
+        open(Path(__file__ + "/../" + "files/test_subject").absolute()), False
+    )
+
+
 def test_simple_1():
-	assert "B resolved as True" == main(open(Path(__file__ + "/../" + "files/test_simple_1").absolute()), False)
+    assert "B resolved as True" == main(
+        open(Path(__file__ + "/../" + "files/test_simple_1").absolute()), False
+    )
 
 
 def test_simple_2():
-	assert "B resolved as True" == main(open(Path(__file__ + "/../" + "files/test_simple_2").absolute()), False)
+    assert "B resolved as True" == main(
+        open(Path(__file__ + "/../" + "files/test_simple_2").absolute()), False
+    )
 
 
 def test_simple_3():
-	assert "B resolved as True" == main(open(Path(__file__ + "/../" + "files/test_simple_3").absolute()), False)
+    assert "B resolved as True" == main(
+        open(Path(__file__ + "/../" + "files/test_simple_3").absolute()), False
+    )
 
 
 def test_simple_4():
-	assert "B resolved as True" == main(open(Path(__file__ + "/../" + "files/test_simple_4").absolute()), False)
+    assert "B resolved as True" == main(
+        open(Path(__file__ + "/../" + "files/test_simple_4").absolute()), False
+    )
 
 
 def test_simple_5():
-	assert "B resolved as True" == main(open(Path(__file__ + "/../" + "files/test_simple_5").absolute()), False)
+    assert "B resolved as True" == main(
+        open(Path(__file__ + "/../" + "files/test_simple_5").absolute()), False
+    )
+
+
+def test_simple_bonus_1():
+    assert "B resolved as True" == main(
+        open(Path(__file__ + "/../" + "files/tests_bonus/test_simple_bonus_1").absolute()), False
+    )
