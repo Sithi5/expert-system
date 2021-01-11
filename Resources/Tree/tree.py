75568b02089411e9d07e34695422464438059b4c
slumfy <valentin.robert.bransat@gmail.com>
2021-01-11 13:50:23 +0100

    rules format 96error

--- Resources/Tree/tree.py
+++ Resources/Tree/tree.py
@@ -6,61 +6,70 @@ LST_OP = {"+": "&", "|": "|", "^": "^"}
 
 
 class Tree:
-    _rules = []
+	_rules = []
 
-    def __init__(self, vb):
-        self.vb = vb
-        self.logger = Logger("Tree", self.vb)
-        self.logger.info("Initialization of class", vb)
-        self.letters = {}
-        self.connectors = []
-        self.root_node = ConnectorNode("+", self)
-        self.root_node.is_root = True
+	def __init__(self, vb):
+		self.vb = vb
+		self.logger = Logger("Tree", self.vb)
+		self.logger.info("Initialization of class", vb)
+		self.letters = {}
+		self.connectors = []
+		self.root_node = ConnectorNode("+", self)
+		self.root_node.is_root = True
 
-    @property
-    def rules(self):
-        return self._rules
+	@property
+	def rules(self):
+		return self._rules
 
-    @rules.setter
-    def rules(self, rules: list):
-        print("Je suis ici ! ")
-        self._rules = rules
+	@rules.setter
+	def rules(self, rules: list):
+		self._rules = []
+		for rule in rules:
+			table = []
+			for l in rule.expression:
+				table.append(l)
+			table.append(rule.implication)
+			for l in rule.result:
+				table.append(l)
+			self._rules.append(table)
 
-    def get_all_letters(self, rules):
-        letter_list = []
-        for rule in rules:
-            for l in rule.expression:
-                if l.isalpha() and l.isupper():
-                    letter_list.append(l)
-            for l in rule.result:
-                if l.isalpha() and l.isupper():
-                    letter_list.append(l)
-        return list(set(letter_list))
+		
+	def get_all_letters(self, rules):
+		letter_list = []
+		for rule in rules:
+			for l in rule.expression:
+				if l.isalpha() and l.isupper():
+					letter_list.append(l)
+			for l in rule.result:
+				if l.isalpha() and l.isupper():
+					letter_list.append(l)
+		return list(set(letter_list))
 
-    def create_all_letternode(self, rules):
-        self.logger.info("Creating LetterNodes")
-        letter_list = self.get_all_letters(rules)
-        self.letters.update(dict((letter, LetterNode(letter, self)) for letter in letter_list))
+	def create_all_letternode(self, rules):
+		self.logger.info("Creating LetterNodes")
+		letter_list = self.get_all_letters(rules)
+		self.letters.update(dict((letter, LetterNode(letter, self)) for letter in letter_list))
 
-    def set_letter_state(self, letter_name, value):
-        letter = self.letters.get(letter_name)
-        if letter is None:
-            self.logger.warning(f"No match found for letter '{letter_name}' in fact section")
-            return
-        letter.state = value
-        if value is True:
-            if letter.state_fixed is True:
-                self.logger.warning(f"Letter already '{letter_name}' set in fact section before")
-            letter.state_fixed = True
+	def set_letter_state(self, letter_name, value):
+		letter = self.letters.get(letter_name)
+		if letter is None:
+			self.logger.warning(f"No match found for letter '{letter_name}' in fact section")
+			return
+		letter.state = value
+		if value is True:
+			if letter.state_fixed is True:
+				self.logger.warning(f"Letter already '{letter_name}' set in fact section before")
+			letter.state_fixed = True
 
-    def set_letters_state(self, rules, facts):
-        self.logger.info("Setting up states")
-        letter_list = self.get_all_letters(rules)
-        for letter in letter_list:
-            self.set_letter_state(letter, False)
-        for fact in facts:
-            self.set_letter_state(fact, True)
-        self.logger.info("End")
+	def set_letters_state(self, rules, facts):
+		self.logger.info("Setting up states")
+		letter_list = self.get_all_letters(rules)
+		for letter in letter_list:
+			self.set_letter_state(letter, False)
+		for fact in facts:
+			self.set_letter_state(fact, True)
+		self.logger.info("End")
 
-    def create_connector_node(self, rules):
-        self.logger.info("create connector node")
+	def create_connector_node(self, rules):
+		self.logger.info("create connector node")
+
--- expert_system.py
+++ expert_system.py
@@ -4,19 +4,20 @@ from Resources.Tree.tree import Tree
 
 
 def main(file, vb):
-    parser = Parser(file, vb)
-    tree = Tree(vb)
-    tree.create_all_letternode(parser.rules)
-    tree.set_letters_state(parser.rules, parser.facts)
-    tree.rules = parser.rules
-    print("tree rules = ", tree.rules)
-    result = "Nothing"
-    return result
+	parser = Parser(file, vb)
+	tree = Tree(vb)
+	tree.create_all_letternode(parser.rules)
+	tree.set_letters_state(parser.rules, parser.facts)
+	tree.rules = parser.rules
+	parser.Parse_rules_to_list(tree)
+	print("tree rules = ", tree.rules)
+	result = "Nothing"
+	return result
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Expert System")
-    parser.add_argument("file", type=argparse.FileType("r"), help="The file containing rules")
-    parser.add_argument("-vb", "--verbose", action="store_true", help="Enable verbose")
-    args = parser.parse_args()
-    main(args.file, args.verbose)
+	parser = argparse.ArgumentParser(description="Expert System")
+	parser.add_argument("file", type=argparse.FileType("r"), help="The file containing rules")
+	parser.add_argument("-vb", "--verbose", action="store_true", help="Enable verbose")
+	args = parser.parse_args()
+	main(args.file, args.verbose)
