5e1f2ef84714327ad0f5088463dd0d0d8946f9b6
ChokMania <judumay@student.42.fr>
2021-01-11 16:35:34 +0100

    creating shell branch

--- Resources/Shell/shell.py
+++ Resources/Shell/shell.py
@@ -1,45 +1,38 @@
 import cmd, sys
+from Resources.Utils.colors import *
 
-BLACK = "\033[30m"
-RED = "\033[31m"
-GREEN = "\033[32m"
-YELLOW = "\033[33m"
-BLUE = "\033[34m"
-PURPLE = "\033[35m"
-CYAN = "\033[36m"
-GREY = "\033[37m"
-END = "\033[0m"
 
 rules = []
 facts = {}
 queries = {}
 
+
 class Shell(cmd.Cmd):
-	intro = 'Welcome to the expert-system shell.   Type help or ? to list commands.\n'
+	intro = "Welcome to the expert-system shell.   Type help or ? to list commands.\n"
 	prompt = f"{GREEN}Expert-System{END}:{BLUE}~{END}$ "
 
 	def do_add_rule(self, arg):
-		'add_rule <rule> : Add a rule'
+		"add_rule <rule> : Add a rule"
 		rules.append(arg)
 
 	def do_add_fact(self, arg):
-		'add_fact <fact(s)> : Add one or more facts'
+		"add_fact <fact(s)> : Add one or more facts"
 		if arg.isalpha():
 			for letter in arg:
-				facts.update({letter.upper() : letter.upper()})
+				facts.update({letter.upper(): letter.upper()})
 		else:
 			print("Fact needs to be alphabeticals characters [A-Z]")
 
 	def do_add_querie(self, arg):
-		'add_fact <querie(s)> : Add one or more queries'
+		"add_fact <querie(s)> : Add one or more queries"
 		if arg.isalpha():
 			for letter in arg:
-				queries.update({letter.upper() : letter.upper()})
+				queries.update({letter.upper(): letter.upper()})
 		else:
 			print("Querie needs to be alphabeticals characters [A-Z]")
 
 	def do_del_rule(self, arg):
-		'del_rule <nb> : Delete rule n°<nb>'
+		"del_rule <nb> : Delete rule n°<nb>"
 		try:
 			rules.pop(int(arg))
 		except Exception:
@@ -50,8 +43,8 @@ class Shell(cmd.Cmd):
 		pass
 
 	def do_del_fact(self, arg):
-		'del_fact <letter> : Delete fact <letter>'
-		try :
+		"del_fact <letter> : Delete fact <letter>"
+		try:
 			facts.pop(arg.upper())
 		except Exception:
 			if len(arg) == 1 and arg.isalpha():
@@ -59,10 +52,9 @@ class Shell(cmd.Cmd):
 			else:
 				print("Fact needs to be an alphabet character [A-Z]")
 
-
 	def do_del_querie(self, arg):
-		'del_querie <letter> : Delete querie <letter>'
-		try :
+		"del_querie <letter> : Delete querie <letter>"
+		try:
 			queries.pop(arg.upper())
 		except Exception:
 			if len(arg) == 1 and arg.isalpha():
@@ -71,33 +63,33 @@ class Shell(cmd.Cmd):
 				print("Querie needs to be an alphabet character [A-Z]")
 
 	def do_reset_rules(self, arg):
-		'reset_rule : Reset all rules'
+		"reset_rule : Reset all rules"
 		rules.clear()
 
 	def do_reset_facts(self, arg):
-		'reset_facts : Reset all facts'
+		"reset_facts : Reset all facts"
 		facts.clear()
 
 	def do_reset_queries(self, arg):
-		'reset_queries : Reset all queries'
+		"reset_queries : Reset all queries"
 		queries.clear()
 
 	def do_reset_all(self, arg):
-		'reset_all : Reset all rules, facts and queries'
+		"reset_all : Reset all rules, facts and queries"
 		self.do_reset_rules(None)
 		self.do_reset_facts(None)
 		self.do_reset_queries(None)
 
 	def do_show_all(self, arg):
-		'show_all : Show all rules, facts and queries'
+		"show_all : Show all rules, facts and queries"
 		self.do_show_rules(None)
 		self.do_show_facts(None)
 		self.do_show_queries(None)
 
 	def do_show_rules(self, arg):
-		'show_rules : Show rules'
+		"show_rules : Show rules"
 		if len(rules) > 0:
-			for idx,rule in enumerate(rules):
+			for idx, rule in enumerate(rules):
 				if arg is not None:
 					print(f"Rule n°{idx}: ", end="")
 				print(f"{rule}")
@@ -105,7 +97,7 @@ class Shell(cmd.Cmd):
 			print("There is no rules to be shown")
 
 	def do_show_facts(self, arg):
-		'show_facts : Show facts'
+		"show_facts : Show facts"
 		if len(facts) > 0:
 			if arg is not None:
 				print("Facts:")
@@ -117,7 +109,7 @@ class Shell(cmd.Cmd):
 			print("There is no facts to be shown")
 
 	def do_show_queries(self, arg):
-		'show_queries : Show queries'
+		"show_queries : Show queries"
 		if len(queries) > 0:
 			if arg is not None:
 				print("Queries:")
@@ -129,14 +121,14 @@ class Shell(cmd.Cmd):
 			print("There is no queries to be shown")
 
 	def do_end(self, arg):
-		'Close the shell window, and launch expert-system'
+		"Close the shell window, and launch expert-system"
 		return True
 
 	def do_exit(self, arg):
-		'Close the shell window, and exit'
+		"Close the shell window, and exit"
 		exit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	Shell().cmdloop()
 	print("Ca continue")
